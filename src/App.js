b5c18beebff731fadd5b18352ef5f4a2c13d70a4
Sachin Subhash <sachin.sbsh2@gmail.com>
2021-09-07 18:38:44 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import APIData from './APIData';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import APIData from "./APIData";
 
 function App() {
-  const [posts,setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1')
-      .then(res => {
-        setPosts(res.data)})
-        .catch(error=> console.log(error))
-      },[]);
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/1`)
+      .then((res) => {
+        setPosts(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const postsFilters = posts.filter((posts) =>
+    posts.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="frontData">
       <div className="data-container">
         <h1>Data from the API</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            className="search-input"
+            onChange={handleChange}
+          />
+        </form>
       </div>
-      <div className="frontData-details">
-        <APIData 
-          ID={posts.id}
-          TITLE={posts.title}
-          BODY={posts.body}/>
-      </div>
+      {postsFilters.map((posts) => {
+        return (
+          <div className="frontData-details">
+            <APIData ID={posts.id} BODY={posts.body} TITLE={posts.title} />
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
