7dccee39b4c11f807b0bdb4f80743b9d2d19f51e
Sachin Subhash <sachin.sbsh2@gmail.com>
2021-09-08 11:06:47 +0400

    Update APIData.js

--- src/APIData.js
+++ src/APIData.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./APIData.css";
 
-const APIData = ({ID, TITLE, BODY}) => {
-    return (
-        <div className="api-container">
-            <div className="api-row">
-                <div className="api">
-                    <h1 className="api-id">{ID}</h1>
-                    <p className="api-title">{TITLE}</p>
-                </div>
-                <div className="api-detail">
-                    <p className="api-body">{BODY}</p>
-                </div>
-            </div>
+const APIData = ({ ID, TITLE, BODY }) => {
+  return (
+    <div className="card-container">
+      <div className="image-container">
+        <img src="" />
+      </div>
+      <div className="card-content">
+        <div className="card-title">
+          <div className="image-container">
+            <img src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+          </div>
+          <h3>Product</h3>
         </div>
-    )
-}
+        <div className="card-body">
+            <h1>{ID}</h1>
+            <p>{BODY}</p>
+        </div>
+        <div className="btn btn-default">
+          <button>
+            <a>View More</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default APIData
+export default APIData;
